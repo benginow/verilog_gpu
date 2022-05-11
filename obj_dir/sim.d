7f62682600c159b0c970ad3463bf9fea54186b40
Aaron Lamoreaux <lamoreauxaj@gmail.com>
2022-05-11 15:08:16 -0500

    added gitignore

--- obj_dir/sim.d
+++
@@ -1,6 +0,0 @@
-sim.o: ../sim.cpp Vvgademo.h \
-  /usr/local/Cellar/verilator/4.222/share/verilator/include/verilated.h \
-  /usr/local/Cellar/verilator/4.222/share/verilator/include/verilatedos.h \
-  /usr/local/Cellar/verilator/4.222/share/verilator/include/verilated_config.h \
-  /usr/local/Cellar/verilator/4.222/share/verilator/include/verilated_types.h \
-  /usr/local/Cellar/verilator/4.222/share/verilator/include/verilated_funcs.h
