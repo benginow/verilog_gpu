9d29194e1bcf0e655f31be0bb41725247267b899
jbenginow <jbenginow@gmail.com>
2022-05-11 17:20:44 -0500

    more infrastructure

--- gpu.v
+++ gpu.v
@@ -8,15 +8,30 @@ begin gpu(      input            clk,
 
 
 //instantiate all four queues
-
+queue transformation(clk, );
+queue lighting(clk, );
+queue projection(clk, );
+queue rasterization(clk, )
 
 //two dedicated memories -- one general purpose, one for instructions
 mem mem(clk, );
-//we only care about reading the instruction mem in the processor, so figure that out, maybe pass instruction to processor
+//we only care about reading the instruction mem in the processor, so figure that out, maybe pass instruction to processor? so processor passes pc in, and the instr_mem passes in instruction
 mem instr_mem(clk, );
+//current registers -- also need to pass this into instruction memory
+reg curr_regs(clk, )
 
 //schedule onto the pipeline (make this parallelized later)
 //pass in program counter
+reg idle;
 processor proc1(clk, );
+
+//zbuffer and framebuffer -- still need to implement these
+
+reg[255:0] register_holder;
+//if the processor is asking for more work(pointed out in the idle object), then 
+always @(posedge clk) begin
+
+end
+
 
 endmodule
--- reg.v
+++ reg.v
@@ -6,16 +6,40 @@ begin reg(input clk
             input[3:0] rin1, output[15:0] rout1,
             input[3:0] rin2, output[15:0] rout2,
             input[3:0] rin3, output[15:0] rout3,
-            input wen, input[3:0] win, input[15:0] wdata);
+            input wen0, input[3:0] win0, input[15:0] wdata0
+            input wen1, input[3:0] win1, input[15:0] wdata1
+            input wen2, input[3:0] win2, input[15:0] wdata2
+            input wen3, input[3:0] win3, input[15:0] wdata3
+            
+            input[2:0] rpred, output predout,
+            input wpreden, input[2:0] wpred, input write_pred_value);
 
     reg[15:0] data[0:15];
+    reg pred[3:0];
 
     always @(posedge clk) begin
         rout0 <= data[rin0];
         rout1 <= data[rin1];
-        if (wen) begin
-            data[win] = wdata;
+        rout2 <= data[rin2];
+        rout3 <= data[rin3];
+        predout <= pred[rpred]
+        if (wen0) begin
+            data[win0] = wdata0;
         end
+        if (wen1) begin
+            data[win1] = wdata1;
+        end
+        if (wen2) begin
+            data[win2] = wdata2;
+        end
+        if (wen3) begin
+            data[win3] = wdata3;
+        end
+        if (wpreden) begin
+            pred[wpred] = wpredvalue;
+        end
+        
+
     end
 
 endmodule
