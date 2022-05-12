c99ba91d61124edede16668911be7afe060d5a24
jbenginow <jbenginow@gmail.com>
2022-05-12 00:11:35 -0500

    rewriting the processor core thing

--- processor.v
+++ processor.v
@@ -1,120 +1,168 @@
 
 //inputs: where in memory is our first instruction (ie do we want to do )
 //          which queue should we add to next (pass in the queue)
-module processor(input clk,
-                input[31:0] d_instr, output[15:0] f_pc,
-                output[3:0] readreg0, input[16:0] inputreg0,
-                output[3:0] readreg1, input[16:0] inputreg1,
+
+//this is not a pipelined core. just a simple joe schmoe core. it'll be ok. dw.
+module processor(
+                input clk,
+
+                output[15:0] pc, input[31:0] instr,
+
+                output[3:0] readreg0, input[15:0] in_reg0,
+                output[3:0] readreg1, input[15:0] in_reg1,
+                
+                output reg_wen, output[3:0] reg_waddr, output[15:0] reg_wval,
+                
+                output[15:0] readmem0, input[15:0] in_mem0,
+                output mem_wen, output[15:0] mem_waddr, output[15:0] mem_wval
                 );
 
-//this will not be pipelined. it takes one clock cycle to access memory (?)
+    //stage one is decode
+    //stage two is read regs
+    //stage three is read/write
+    reg[2:0] stage = 0;
 
-//read this from instr mem
-reg[15:0] f_pc;
-//fetch instruction
-always @(posedge clk) begin
-    f_pc <= f_pc + 1;
-end
+    //stage 1 variables
+    reg[1:0] pred;
+    reg optype;
+    reg[4:0] opcode;
+    reg[3:0] reg0;
+    reg[3:0] reg1;
+    reg[3:0] targetreg;
+    reg[15:0] constant;
 
-//decode instruction
-//i think the instruction should be passed in from gpu.v
-always @(posedge clk) begin
-    //print these later to make sure decode is working
-    r_pred <= d_instr[31:30];
-    r_optype <= d_instr[29]
-    r_opcode <= d_instr[28:24];
-    r_reg1 <= d_instr[23:20];
-    r_reg2 <= d_instr[19:16];
-    r_constant <= d_instr[15:0];
-end
 
-//read from registers as needed
-reg[1:0] r_pred;
-reg r_optype;
-reg[4:0] r_opcode;
-reg[3:0] r_reg1;
-reg[3:0] r_reg2;
-reg[15:0] r_constant;
-always @(posedge clk) begin
-    //load from memory
-    if (r_opcode == 0) begin
-        //need to read from reg then load memory next cycle
-    end
-    //store in memory
-    if (r_opcode == 1) begin
-        //need to read from reg then store in memory next cycle
-    end
-    //multiply
-    if (r_opcode == 2) begin
-        //need to read from reg then store in reg next cycle
+    //stage 2 variables
+    reg[15:0] reg0val;
+    reg[15:0] reg1val;
+
+
+    //i/o variables
+    wire[3:0] readreg0 = reg1;
+    wire[3:0] readreg1 = reg2;
+
+    always @(posedge clk) begin
+
+
+        if (stage == 0) begin 
+            pred <= instr[31:30];
+            optype <= instr[29]
+            opcode <= instr[28:24];
+            reg0 <= instr[23:20];
+            reg1 <= instr[19:16];
+            targetreg <= instr[15:12];
+            constant <= instr[15:0];
+            stage <= stage + 1;
+
+            //make sure you're no longer writing
+            reg_wen <= 0;
+            mem_wen <= 0;
+        end
+
+        //read registers
+        if (stage == 1) begin
+            reg0val <= in_reg0;
+            reg1val <= in_reg1;
+            stage <= stage + 1;
+        end
+
+        //read from memory as needed
+        if (stage == 2) begin
+            readmem0 <= reg0val;
+        end
+
+        if (stage == 3) begin
+                //load from memory
+            if (r_opcode == 0) begin
+                //need to read from reg then load memory next cycle
+                //TODO: DOUBLE CHECK THIS. MAKE SURE ASSEMBLER WORKS!
+                reg_wen <= 1;
+                reg_waddr <= targetreg;
+                reg_wval <= readmem0;
+            end
+            //store in memory
+            if (r_opcode == 1) begin
+                mem_wen <= 1;
+                mem_waddr <= reg1val;
+                mem_wval <= reg0val;
+            end
+            //multiply
+            if (r_opcode == 2) begin
+                //need to read from reg then store in reg next cycle
+                reg_wen <= 1;
+                reg_waddr <= targetreg;
+                reg_wval <= reg0val * reg1val;
+            end
+            //add
+            if (r_opcode == 3) begin
+                reg_wen <= 1;
+                reg_waddr <= targetreg;
+                reg_wval <= reg0val + reg1val;
+            end
+            //sub
+            if (r_opcode == 4) begin
+                reg_wen <= 1;
+                reg_waddr <= targetreg;
+                reg_wval <= reg0val - reg1val;
+            end
+            //srl
+            if (r_opcode == 5) begin
+                reg_wen <= 1;
+                reg_waddr <= targetreg;
+                reg_wval <= reg0val >> reg1val;
+            end
+            //sll
+            if (r_opcode == 6) begin
+                reg_wen <= 1;
+                reg_waddr <= targetreg;
+                reg_wval <= reg0val << reg1val;
+            end
+            //and
+            if (r_opcode == 7) begin
+                reg_wen <= 1;
+                reg_waddr <= targetreg;
+                reg_wval <= reg0val & reg1val;
+            end
+            if (r_opcode == 8) begin
+                
+            end
+            if (r_opcode == 9) begin
+
+            end
+            if (r_opcode == 10) begin
+
+            end
+            if (r_opcode == 11) begin
+
+            end
+            if (r_opcode == 12) begin
+                
+            end
+            if (r_opcode == 13) begin
+
+            end
+            if (r_opcode == 14) begin
+
+            end
+            //store in queue based off of number
+            //need to communicate this to gpu, since gpu keeps track of all of this
+            if (r_opcode == 15) begin
+
+            end
+            //store in queue based off of register
+            //need to communicate this to gpu so it may do it
+            if (r_opcode == 16) begin
+                
+            end
+            //end
+            if (r_opcode == 17) begin
+
+            end
+
+            stage <= 0;
+        end
 
     end
-    if (r_opcode == 3) begin
 
-    end
-    if (r_opcode == 4) begin
-
-    end
-    if (r_opcode == 5) begin
-        
-    end
-    if (r_opcode == 6) begin
-
-    end
-    if (r_opcode == 7) begin
-        
-    end
-    if (r_opcode == 8) begin
-
-    end
-    if (r_opcode == 9) begin
-
-    end
-    if (r_opcode == 10) begin
-
-    end
-    if (r_opcode == 11) begin
-
-    end
-    if (r_opcode == 12) begin
-        
-    end
-    if (r_opcode == 13) begin
-
-    end
-    if (r_opcode == 14) begin
-
-    end
-    //store in queue based off of number
-    //need to communicate this to gpu, since gpu keeps track of all of this
-    if (r_opcode == 15) begin
-
-    end
-    //store in queue based off of register
-    //need to communicate this to gpu so it may do it
-    if (r_opcode == 16) begin
-        
-    end
-    //end
-    if (r_opcode == 17) begin
-
-    end
-end
-
-//execution stage
-//read from memory as needed, store in memory as needed
-//store in register as needed
-reg[15:0] e_reg_data;
-reg[15:0] e_reg_store;
-
-reg[1:0] e_pred;
-reg e_optype;
-reg[4:0] e_opcode;
-reg[3:0] e_reg1;
-reg[3:0] e_reg2;
-reg[15:0] e_constant;
-always @(posedge clk) begin
-
-end
 
 endmodule
